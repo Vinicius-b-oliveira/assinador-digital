c4c022b35b0d8849667ff2e17d6be91d3d9007a1
Vinicius Bueno <viniciusbuenodeoliveira2017@gmail.com>
2026-06-24 20:23:14 -0300

    feat: adiciona seeders de ambiente local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        // $this->call([
-        //     UserSeeder::class,
-        //     DocumentSeeder::class,
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            DocumentSeeder::class,
+        ]);
     }
 }
